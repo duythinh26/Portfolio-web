b0bfd21f2bd17e69079d2248de106308afb246a2
duythinh26 <pduythinh79@gmail.com>
2025-01-12 14:02:55 +0700

    changed file name

---
+++ code/src/sections/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-center items-center relative top-3">
+      <nav>
+        <a href="#" className="nav-item">Home</a>
+        <a href="#" className="nav-item">Projects</a>
+        <a href="#" className="nav-item">About</a>
+        <a href="#" className="nav-item">Contact</a>
+      </nav>
+    </div>
+  )
+};
